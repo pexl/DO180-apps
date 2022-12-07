9971fef7d26852371d0aad9434505d442e875a34
Student User <student@workstation.lab.example.com>
2022-12-07 10:24:27 -0500

    updated app

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,8 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "..and another line in the file: " "\n"; 
+print "Children begin by loving their parents.  After a time they judge them " "\n";
+print "Rarely, if ever, do they forgive them " "\n";
+print "-- Oscar Wild " "\n";
 ?>
+
